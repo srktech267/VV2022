249c0fbf6cca79f433c122aa8ff2ed25cd0a34c3
unexpectedproton <unexpectedproton@gmail.com>
2022-10-15 15:01:05 -0400

    test to Ian's branch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VVTeleOpFour.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VVTeleOpFour.java
@@ -49,7 +49,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
-
+//Ian testing Git-something
 @TeleOp(name="VVTeleOpFour*", group="Tele-op")
 
 public class VVTeleOpFour extends LinearOpMode {
